b2ff5586efd72cb00755322794bafa6db7ac40fd
agbogoslavskaya <agbogoslavskaya@MSK-C02DL715MD6R.local>
2024-06-25 23:59:10 +0300

    notebooks with 2 setup conditions

--- models/pernir_user_imitation.py
+++ models/pernir_user_imitation.py
@@ -13,9 +13,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 from collections import defaultdict
 import ast
 import pickle
+import pandas as pd
+import numpy as np
 
 class Collab:
-    def __init__(self, train_baskets, test_samples,train_url, alpha1=1, alpha2=None):
+    def __init__(self, train_baskets, test_samples, train_url, alpha1=1, alpha2=None):
         self.train_baskets = train_baskets
         self.test_samples = test_samples
         self.alpha1 = alpha1
@@ -83,7 +85,7 @@ class Collab:
 
         return final_item_scores
 
-    def predict(self, user, basket, top_n=20):
+    def predict(self, user, basket, masked_basket=[], previously_predicted_items=set(), top_n=5):
         personal_scores = self.user_predictions(user, basket)
         neighbor_scores = {}
         for neighbor in self.user_neighbors.get(user, []):
@@ -112,8 +114,9 @@ class Collab:
                 final_item_scores[item] = 0
             final_item_scores[item] += beta2 * (agg_neighbor_scores[item] / norm_term[item])
 
-         #filtering the recommednations that were added to the basket with the items that already 've been in the basket
-        filtered_item_scores = {item: score for item, score in final_item_scores.items() if item not in basket}
+        # Exclude items in masked_basket and previously predicted items
+        filtered_item_scores = {item: score for item, score in final_item_scores.items()
+                                if item not in masked_basket and item not in previously_predicted_items}
 
         sorted_item_scores = sorted(filtered_item_scores.items(), key=lambda x: x[1], reverse=True)
         predicted_items = [x[0] for x in sorted_item_scores[:top_n]]
